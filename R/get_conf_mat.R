9a5a69d5d9018f91a2662c7554988372491b61e6
Mitchell Lyons <mitchell.lyons@gmail.com>
2017-07-20 16:09:09 +1000

    typos and attributes

--- R/get_conf_mat.R
+++ R/get_conf_mat.R
@@ -7,7 +7,7 @@
 #' @param make.A.hard logical (defaults to FALSE). If TRUE, and if A= is a matrix of soft membership, it will be degraded to a hard binary matrix, taking the highest value, breaking ties at random
 #' @param make.B.hard logical (defaults to FALSE). If TRUE, and if B= is a matrix of soft membership, it will be degraded to a hard binary matrix, taking the highest value, breaking ties at random
 #'
-#' @details Takes inputs A and B (converting labels to matrices if required) and combines them via (\eqn{A^TB}). Soft classifications will neccesarily be matrices. Hard classifications can be given as a binary matrix of membership or a vector of labels. For matrix inputs, rows should represent individual sites, observations, cases etc., and columns should represent classes. For class label inputs, the vector should be ordered similar by site, observation, case etc; they will be converted to a binary matrix (see \code{\link[c2c]{labels_to_matrix}}). Classes from matrix A are represented by columns of the output, and classes from matrix B are represented by the rows. Class names inherited from \code{names()} or \code{colnames()}.
+#' @details Takes inputs A and B (converting labels to matrices if required) and combines them via (\eqn{A^TB}). Soft classifications will neccesarily be matrices. Hard classifications can be given as a binary matrix of membership or a vector of labels. For matrix inputs, rows should represent individual sites, observations, cases etc., and columns should represent classes. For class label inputs, the vector should be ordered similarly by site, observation, case etc; they will be converted to a binary matrix (see \code{\link[c2c]{labels_to_matrix}}). Classes from matrix A are represented by rows of the output, and classes from matrix B are represented by the columns Class names inherited from \code{names()} or \code{colnames()}.
 #'
 #' @return A confusion matrix
 #'
@@ -25,6 +25,7 @@
 #' @export
 
 get_conf_mat <- function(A, B, make.A.hard = F, make.B.hard = F) {
+
   if (length(dim(A)) < 2) {
     Amat <- labels_to_matrix(A)
   } else {
@@ -38,6 +39,8 @@ get_conf_mat <- function(A, B, make.A.hard = F, make.B.hard = F) {
     if("character" %in% apply(Bmat, 2, class)) {stop("Argument B= contained characters - if supplying a matrix (or data.frame or similar) all columns should be numeric or integer")}
   }
 
+  if(nrow(Amat) != nrow(Bmat)) {stop("Matrices or label vectors indicate differing number of observations - they must be equal")}
+
   if (make.A.hard) {
     Amat <- get_hard(Amat)
   } else {
@@ -50,5 +53,10 @@ get_conf_mat <- function(A, B, make.A.hard = F, make.B.hard = F) {
     Bmat <- na_to_zero(Bmat)
   }
 
-  t(Amat) %*% Bmat
+  conf_mat <- t(Amat) %*% Bmat
+
+  class(conf_mat) <- c("confmat", "matrix")
+  attr(conf_mat, "equal_dims") <- ncol(Amat) == ncol(Bmat)
+
+  conf_mat
 }
